f4a277d501f9dc5e7f7430230e8cb92697be54a2
Jeremy-Tchao <Jeremyzao@gmail.com>
2019-03-17 15:35:47 -0700

    Force directed diagra integrated

--- scripts/force_chart.js
+++ scripts/force_chart.js
@@ -1,8 +1,210 @@
-var path;
-var countrySel = "China";
+var boundingBoxForce = d3.select("#force").node().getBoundingClientRect();
 
-function drawForce(data, field) 
+
+var forceWidth = boundingBoxForce.width,
+    forceHeight = boundingBoxForce.height;
+
+
+// let graph = 
+// {
+//   "nodes": [
+//     {"id": 1},
+//     {"id": 2},
+//     {"id": 4},
+//     {"id": 8},
+//     {"id": 16},
+//   ],
+//   "links": [
+//     {"source": 2, "target": 1, "value": 1 , "distance": 30},
+//     {"source": 4, "target": 1, "value": 1, "distance": 15},
+//     {"source": 4, "target": 1, "value": 1, "distance": 50},
+//     {"source": 8, "target": 1, "value": 1, "distance": 60},
+//     {"source": 16, "target": 1, "value": 1, "distance": 100}
+//   ]
+// }
+
+function drawForce(data, country, selection) 
 {
+
+    var svgForce = d3.select("#force");
+
   
-};
-  
+
+    var simulation = d3.forceSimulation()
+        .force("link", d3.forceLink().distance(linkDistance).id(function(d) { return d.id; }))
+        .force('charge', d3.forceManyBody()
+        .strength(-1900)
+        .theta(-0.3)
+        )
+        .force("center", d3.forceCenter(forceWidth / 2, forceHeight / (4.3)))
+
+
+    d3.select('#force').selectAll('g').remove();
+    selection = "Economy..GDP.per.Capita."
+    // console.log(data[1]);
+
+    var key = 0;
+    // graph = [];
+    var graph = {"nodes": [], "links":[]};
+    var indexArr = [];
+    
+    graph.nodes.push({"id": country, "name": data[1][country]["Country"], "value": data[1][country][selection]});
+
+    if(country < 5)
+    {
+        key = 1;
+        var j = 5-country;
+        
+        
+        for(var i = 1; i < 6 + j; i++) 
+        {
+            graph.nodes.push({"id": country + i, "name": data[1][country + i]["Country"], "value": data[1][country + i][selection]});
+
+            graph.links.push({"source": country + i, "target": country, "distance": data[1][country + i][selection]})
+
+        }
+
+        for(var i = 1; i < country; i++)
+        { 
+            graph.nodes.push({"id": country - i, "name": data[1][country - i]["Country"], "value": data[1][country - i][selection]});
+
+            graph.links.push({"source": country - i, "target": country, "distance": data[1][country - i][selection]});
+        }
+    }
+
+    if(country > 149)
+    {
+        key = 1;
+        var j = (154 - country) + 5 ;
+
+        
+        for(var i = country; i < 154; i++) 
+        {
+            graph.nodes.push({"id": country + i, "name": data[1][country + i]["Country"], "value": data[1][country+i][selection]});
+
+            graph.links.push({"source": country + i, "target": country,  "distance": data[1][country + i][selection]})
+
+        }
+
+        for(var i = 1; i < 6 + j; i++)
+        { 
+            graph.nodes.push({"id": country - i, "name": data[1][country - i]["Country"], "value": data[1][country - i][selection]});
+
+            graph.links.push({"source": country - i, "target": country, "distance": data[1][country - i][selection]});
+        }
+    }
+
+    for(var i = 1; i < 6; i++)
+    {  
+        if(key == 1) break;
+
+        graph.nodes.push({"id": country - i, "name": data[1][country - i]["Country"], "value": data[1][country - i][selection]});
+
+        graph.links.push({"source": country - i, "target": country, "distance": data[1][country - i][selection]});
+
+        graph.nodes.push({"id": country + i, "name": data[1][country + i]["Country"], "value": data[1][country + i][selection]});
+
+        graph.links.push({"source": country + i, "target": country, "distance": data[1][country + i][selection]})
+    }
+
+    
+
+
+    var link = svgForce.append("g")
+                .style("stroke", "#aaa")
+                .selectAll("line")
+                .data(graph.links)
+                .enter().append("line");
+
+    var node = svgForce.append("g")
+            .attr("class", "nodes")
+    .selectAll("circle")
+            .data(graph.nodes)
+    .enter().append("circle")
+            .attr("r", 24)
+            .on("mouseover", function(d){
+            d3.select(this)
+            .classed("activeCountry", true)
+            tooltip.transition()    
+            .duration(200)    
+            .style("opacity", 1);    
+            tooltip
+            .style("font", "12px sans-serif")
+            .style("background", "lightsteelblue")
+            .style("text-align", "center")
+            .style("height", "40px")
+            .html(function()
+            {
+                console.log(d);
+                if(d.value != undefined)
+                return d.name + " <br> " + selection + ": <br>" + d.value
+                else 
+                return d.name + " <br> Happiness Score: <br> No Data"
+            })  
+            .style("left", (d3.event.pageX) + "px")   
+            .style("top", (d3.event.pageY - 33) + "px");}
+            )
+        .on("mouseout", function(d){
+            d3.select(this)
+            .classed("activeCountry", false)
+            tooltip.transition()    
+            .duration(500)    
+            .style("opacity", 0);}
+            )
+        .on("mousemove", function(){
+            tooltip
+                .style("top", (d3.event.pageY - 10) + "px" )
+                .style("left", (d3.event.pageX + 10) + "px");}
+              )
+            
+    var label = svgForce.append("g")
+        .attr("class", "labels")
+        .selectAll("text")
+        .data(graph.nodes)
+        .enter().append("text")
+        .attr("class", "label")
+        .text(function(d) { return d.id; });
+
+    simulation
+        .nodes(graph.nodes)
+        .on("tick", ticked);
+
+    simulation.force("link")
+        .links(graph.links);
+
+
+
+    redrawMap(graph.nodes[0], graph.nodes);
+
+    function ticked() {
+    link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node
+            .attr("r", 21)
+            .style("fill", "#efefef")
+            .style("stroke", "#424242")
+            .style("stroke-width", "1px")
+            .attr("cx", function (d) { return d.x+5; })
+            .attr("cy", function(d) { return d.y-3; });
+
+    label
+            .attr("x", function(d) { return d.x; })
+            .attr("y", function (d) { return d.y; })
+            .style("font-size", "10px").style("fill", "#333");
+  }
+
+
+}
+
+
+
+function linkDistance(d) 
+{
+
+    
+    return (d.distance) * 90;
+}
--- scripts/map_visualization.js
+++ scripts/map_visualization.js
@@ -31,36 +31,36 @@ var projection = d3.geoMercator()
 var zoom = d3.zoom().on("zoom", zoomed);
 var path = d3.geoPath().projection(projection);
 
-
 var svg = d3.select("#map").append("svg")
     .attr("width", width)
     .attr("height", height)
     .on("click", stopped, true);
 
-svg.append("rect")
+    svg.append("rect")
     .attr("class", "background")
     .attr("width", "100%")
     .attr("height", "100%")
     .on("click", reset);
 
-var g = svg.append("g");
+    var g = svg.append("g");
 
-svg
-    .call(zoom);
+    svg
+        .call(zoom);
+
 
 
 var url1 = "./data/world_countries.json";
 var url2 = "./data/2017.csv";
+// var url3 = "./data/data.json";
 
 var q = d3_queue.queue(1)
     .defer(d3.json, url1)
     .defer(d3.csv, url2)
+    // .defer(d3.json, url3)
     .awaitAll(drawMap);
 
 
 /******************************Global Vars *********************/
-
-
 
 /******************************Legends *********************/
 var legendHeight = 1000;
@@ -118,31 +118,26 @@ legendSvg.append("g")
 .style("text-anchor", "left")
 .text("Happiness Score");
 
+
 /******************************Legends *********************/
 
 
 /******************************Functions*********************/
 
 
-function drawMap(error, data, fieldMain, fieldSub) 
+function drawMap(error, data) 
 {
-    var selection = "United States"
+
+
+    var selection = "United States";
     var index;
     g.selectAll("path")
         .data(data[0].features)
         .enter().append("path")
         .attr("class", "country")
-        .attr("stroke", function(d)
-        {   
-            return "white";
-        })
-        .attr("stroke-opacity", function(d)
-        {   
-            if(d.properties.name == selection) return 0.5;
-            else if(d.properties.name == fieldSub) return 0.5;
-            else return 0.5;
-        })
-        .attr("stroke-width", "0.4px")
+        .attr("stroke", "white")
+        .attr("stroke-opacity",0.5)
+        .attr("stroke-wdith", "0.4px")
         .attr("d", path)
         .attr("class", "feature")
         .attr("fill",function(d)
@@ -152,14 +147,38 @@ function drawMap(error, data, fieldMain, fieldSub)
                 if (d.properties.name == "Greenland")
                 return (p.Country == "Denmark")
                 else return (p.Country == d.properties.name)});
-            if(index != -1) d.happiness = data[1][index]
-            ["Happiness.Score"];
+            if(index != -1)
+            {
+                d.happiness = data[1][index]["Happiness.Score"];
+                d.index = index;
+            }
+            
             else return "grey"
             // console.log(d.happiness)     
             return color(d.happiness);
       
         })
-        .on("click", clicked)
+        .on("click", function(d){
+            if (active.node() === this) return reset();
+            active.classed("active", false);
+            active = d3.select(this).classed("active", true);
+        
+            var bounds = path.bounds(d),
+                dx = bounds[1][0] - bounds[0][0],
+                dy = bounds[1][1] - bounds[0][1],
+                x = (bounds[0][0] + bounds[1][0]) / 2,
+                y = (bounds[0][1] + bounds[1][1]) / 2,
+                scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / width, dy / height))),
+                translate = [width / 2 - scale * x, height / 2 - scale * y];
+        
+                svg.transition()
+                .duration(750)
+                // .call(zoom.translate(translate).scale(scale).event); // not in d3 v4
+                .call( zoom.transform, d3.zoomIdentity.translate(translate[0],translate[1]).scale(scale) ); // updated for d3 v4
+
+            drawForce(data, d.index, "rank")
+            }
+        )
         .on("mouseover", function(d){
             d3.select(this)
             .classed("activeCountry", true)
@@ -195,28 +214,46 @@ function drawMap(error, data, fieldMain, fieldSub)
               )
 };
 
-function clicked(d) 
+
+function redrawMap(fieldMain, fieldSub)
 {
-    if (active.node() === this) return reset();
-    active.classed("active", false);
-    active = d3.select(this).classed("active", true);
-  
-    var bounds = path.bounds(d),
-        dx = bounds[1][0] - bounds[0][0],
-        dy = bounds[1][1] - bounds[0][1],
-        x = (bounds[0][0] + bounds[1][0]) / 2,
-        y = (bounds[0][1] + bounds[1][1]) / 2,
-        scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / width, dy / height))),
-        translate = [width / 2 - scale * x, height / 2 - scale * y];
-  
-    svg.transition()
-        .duration(750)
-        // .call(zoom.translate(translate).scale(scale).event); // not in d3 v4
-        .call( zoom.transform, d3.zoomIdentity.translate(translate[0],translate[1]).scale(scale) ); // updated for d3 v4
+
+    svg.selectAll(".feature")
+    .classed("active", function(d)
+        {   
+            
+            if(fieldMain == undefined)
+            {return false;}
+
+            
+            for(var i = 0; i < fieldSub.length; i++)
+            {
+                // console.log(fieldSub[i]["name"]);
+                if(d.properties.name == fieldSub[i]["name"]) return true;
+            }
+
+            if(d.properties.name == fieldMain["name"]) return true;
+
+            // else {return false;}
+            
+            
+
+        })
+
+
+
+        //  function(d)
+        // {   
+        //     if(d.properties.name == selection) return 0.5;
+        //     else if(d.properties.name == fieldSub) return 0.5;
+        //     else return 0.5;
+        // }
+
 }
-  
+
 function reset() 
 {
+    active = d3.select("#map").selectAll(".active")
     active.classed("active", false);
     active = d3.select(null);
   
